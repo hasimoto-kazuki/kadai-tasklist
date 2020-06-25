5c4c993a32f0b2d858ebb7935f240f02b7502f48
kazuki <kitatanabe0709@gmail.com>
2020-06-25 11:45:41 +0000

    CRUD tasks

---
+++ resources/views/tasks/index.blade.php
@@ -0,0 +1,26 @@
+@extends('layouts.app')
+
+@section('content')
+
+    <h1>仕事一覧</h1>
+
+    @if (count($tasks) > 0)
+        <table class="table table-striped">
+            <thead>
+                <tr>
+                    <th>id</th>
+                    <th>仕事</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($tasks as $task)
+                <tr>
+                    <td>{!! link_to_route('tasks.show', $task->id, ['task' => $task->id]) !!}</td>
+                    <td>{{ $task->content }}</td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    @endif
+
+@endsection
